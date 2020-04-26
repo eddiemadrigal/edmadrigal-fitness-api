1fc7cd8a552c21a47c897a43042c7f7e869c9d6f
Eddie Madrigal <edmadrigal@yahoo.com>
2020-04-26 13:57:53 -0700

    env refactored

--- knexfile.js
+++ knexfile.js
@@ -27,13 +27,18 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
-    connection: process.env.DB_URL,
-    migrations: {
-      directory: "./data/migrations"
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
     },
-    seeds: {
-      directory: "./data/seeds"
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
-  },
+  }
 };
