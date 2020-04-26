1c4468c5677474ca8ad81b06a7c4aba1e1f35831
Eddie Madrigal <edmadrigal@yahoo.com>
2020-04-26 14:05:36 -0700

    index.js updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+// require('dotenv').config();
 const server = require('./api/server.js');
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
